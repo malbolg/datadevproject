31100a0f341ca4c8f0befd1f3aa8e4011a98dc1e
malbolg <malbolg@gmail.com>
2015-02-22 20:52:13 +0200

    Changed title name from example to regression coefs

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ shinyUI(navbarPage("Regression Example",
         
         
         pageWithSidebar(
-  headerPanel("Example plot"),
+  headerPanel("Regression coeffs"),
   sidebarPanel(
     sliderInput('intercept', 'Adjust the intercept',value = 115, min = 100, max = 115, step = 0.05,),      
     sliderInput('slope', 'Adjust the slope',value = 0, min = 0, max = 2, step = 0.01,)
