fdbb8840296a132a48275e6848066bc1636b3153
malbolg <malbolg@gmail.com>
2015-02-22 19:15:54 +0200

    initial submission

--- ui.R
+++ ui.R
@@ -2,8 +2,8 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Example plot"),
   sidebarPanel(
-    sliderInput('intercept', 'Guess the intercept',value = 106, min = 100, max = 110, step = 0.05,),      
-    sliderInput('slope', 'Guess the slope',value = 1.4, min = 0, max = 2, step = 0.01,)
+    sliderInput('intercept', 'Adjust the intercept',value = 115, min = 100, max = 115, step = 0.05,),      
+    sliderInput('slope', 'Adjust the slope',value = 0, min = 0, max = 2, step = 0.01,)
   ),
   mainPanel(
     plotOutput('newPlot'),
